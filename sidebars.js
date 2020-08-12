42d4d4ebb3567cd090e7e1cab4b8270acc28597e
BillyDaPoop <65975856+BillyDaPoop@users.noreply.github.com>
2020-08-12 00:31:04 -0700

    how to sftp
    hopefully works

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    'Basics': ['basic-controls', 'scheduling-actions', 'editing-account-details', 'pointing-a-domain'],
+    'Basics': ['basic-controls', 'scheduling-actions', 'how-to-use-SFTP', 'editing-account-details', 'pointing-a-domain'],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
