937774d13dc010d8ae303b08c77f7f8e0c6efb75
BillyDaPoop <65975856+BillyDaPoop@users.noreply.github.com>
2020-08-11 17:19:35 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
   someSidebar: {
-    'Bloom Basics': ['Basic Controls', 'Scheduling Commands', 'Editing Account Details', 'Pointing A Domain'], 
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
